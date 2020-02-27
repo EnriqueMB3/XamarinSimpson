d790c60075cdb651f6ad530266cda9216978a3a8
Alejandra Robledo <alerobledo710@outlook.com>
2020-02-26 20:34:41 -0600

    INotifyPropertyChanged Agregado

--- SimpsonApp/SimpsonApp/Models/Episodio_M.cs
+++ SimpsonApp/SimpsonApp/Models/Episodio_M.cs
@@ -1,21 +1,89 @@
 ﻿using SQLite;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace SimpsonApp.Models
 {
-   public class Episodio_M
+   public class Episodio_M : INotifyPropertyChanged
     {
-        public string Imagen { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void Actualizar([CallerMemberName]string nombre = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nombre));
+        }
+
+        private string img;
+
+        public string Imagen
+        {
+            get { return img; }
+            set { img = value; Actualizar(); }
+        }
+
+        private int id;
+
         [PrimaryKey]
-        public int Id { get; set; }
-        public string Nombre { get; set; }
-        public string NombreOriginal { get; set; }
-        public DateTime FechaEmision { get; set; }
-        public int Duracion { get; set; }
-        public string Sinopsis { get; set; }
-        public int NumeroTemporada { get; set; }
-        public int NumeroEpisodio { get; set; }
+        public int Id
+        {
+            get { return id; }
+            set { id = value; Actualizar(); }
+        }
+        private string nombre;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; Actualizar(); }
+        }
+        private string nomOriginal;
+
+        public string NombreOriginal
+        {
+            get { return nomOriginal; }
+            set { nomOriginal = value; Actualizar(); }
+        }
+        private DateTime fecha;
+
+        public DateTime FechaEmision
+        {
+            get { return fecha; }
+            set { fecha = value; Actualizar(); }
+        }
+
+        private int duracion;
+
+        public int Duracion
+        {
+            get { return duracion; }
+            set { duracion = value; Actualizar(); }
+        }
+        private string sinopsis;
+
+        public string Sinopsis
+        {
+            get { return sinopsis; }
+            set { sinopsis = value; Actualizar(); }
+        }
+
+        private int numTemporada;
+
+        public int NumeroTemporada
+        {
+            get { return numTemporada; }
+            set { numTemporada = value; Actualizar(); }
+        }
+
+        private int numEpisodio;
+
+        public int NumeroEpisodio
+        {
+            get { return numEpisodio; }
+            set { numEpisodio = value; Actualizar(); }
+        }
+
     }
 }
--- SimpsonApp/SimpsonApp/Models/Temporada_M.cs
+++ SimpsonApp/SimpsonApp/Models/Temporada_M.cs
@@ -1,33 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace SimpsonApp.Models
 {
-   public class Temporada_M
+   public class Temporada_M : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void Actualizar([CallerMemberName]string nombre = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nombre));
+        }
         private int numTemp;
+        private int episodio;
+        private string titulo;
 
         public int NumeroTemporada
         {
             get { return numTemp; }
-            set { numTemp = value; }
+            set { numTemp = value; Actualizar(); }
         }
-
-        private int episodio;
 
         public int NumeroEpisodio
         {
             get { return episodio; }
-            set { episodio = value; }
+            set { episodio = value; Actualizar(); }
         }
-
-        private string titulo;
 
         public string TituloEpisodio
         {
             get { return titulo; }
-            set { titulo = value; }
+            set { titulo = value; Actualizar(); }
         }
 
         // public string imagen { get; set; }
